4a58d047927bb6f088ff675d8bae68d48b1a922e
ichenwu <icw3@uw.edu>
2015-10-15 19:24:49 -0700

    added scroll add

--- frames/app/js/components/DropzoneCustom.js
+++ frames/app/js/components/DropzoneCustom.js
@@ -19,7 +19,10 @@ var DropzoneCustom = React.createClass({
         return {
           images: [],
           modalIsOpen: false,
-          modalIndex: 0
+          modalIndex: 0,
+          uploading: false,
+          skip:0,
+          fetching: false
         };
     },
     openModal: function(index) {
@@ -34,9 +37,9 @@ var DropzoneCustom = React.createClass({
         for (var i = files.length - 1; i >= 0; i--) {
             this.upload(files[i]);
         };
-        // this.setState({
-        //     files: files
-        // });
+        this.setState({
+            uploading: true
+        });
     },
 
     upload: function (file) {
@@ -46,23 +49,37 @@ var DropzoneCustom = React.createClass({
         //     pictureHub.push(dataURL);
         // };
         // reader.readAsDataURL(file);
+        var self = this;
         console.log(file);
         var fileName = file.name;
         var parseFile = new Parse.File(fileName, file);
         parseFile.save().then(function() {
             var image = new Parse.Object("image");
             image.set('image',parseFile);
-            image.save();
+            return image.save();
+        }).then(function(){
+            self.setState({
+                uploading: false,
+                images: []
+            })
+            console.log('done uploading');
+            self.loadPictures();
         });
 
     },
 
-    componentDidMount: function () {
-  $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-  });
-          
+    componentDidMount: function () {  
+        this.loadPictures();
+        var self = this;
+        $('.gallery').scroll(function() {
+            if($('.gallery').scrollTop() > 150 && !self.state.fetching){
+                self.loadPictures();
+            }
+        })
+
+    },
+    loadPictures: function() {
+
         var self = this;
         // pictureHub.limitToLast(3).on("child_added", function(snapshot) {
         //     self.state.files.push(snapshot.val())
@@ -74,20 +91,28 @@ var DropzoneCustom = React.createClass({
         var image = Parse.Object.extend("image");
         var queryObject = new Parse.Query(image);
         var self = this;
-
+        queryObject.limit(6);
+        queryObject.skip(this.state.skip);
+        this.setState({
+            fetching: true
+        })
         queryObject.find().then(function(results){
-                for (var i = 0; i < results.length; i++) {
+            // console.log(results);
+                for (var i = results.length - 1; i >= 0; i--) {
                     var photo = results[i].get('image');
                     var photoUrl = photo.url();
                     self.state.images.push({id:results[i].id,url:photoUrl,text:results[i].get('text')})
-                    console.log(self.state.images);
-                }            
+                    // console.log(self.state.images);
+                }          
+                return results.length;
+        }).then(function(length){
+            if(length>0){
                 self.setState({
-                    staffs: self.state.staffs
+                    skip: self.state.skip + 6,
+                    fetching: false
                 })  
-
+            }  
         })
-
     },
 
     onOpenClick: function () {
@@ -110,10 +135,14 @@ var DropzoneCustom = React.createClass({
         return (
             <div style={{height: '90vh'}}>
                 <div style={{height: '10vh', backgroundColor:'#e91e63', fontSize:'20px', padding:'10px', color:'white', fontWeight:'100'}}>Pictures</div>
-                <div style={{overflow:'scroll',height:'100%',justifyContent:'space-between', padding:'15px', display:'flex',flexWrap: 'wrap'}}>
+                <div className='gallery' style={{overflow:'scroll',height:'90%',justifyContent:'space-between', padding:'15px', display:'flex',flexWrap: 'wrap'}}>
+                          {this.state.uploading && (<div className="progress">
+                              <div className="indeterminate"></div>
+                          </div>)}
                         <div>
-                            <Dropzone style={{margin:'5px',border:'2px #eee solid', width: '120px', height: '120px', display:'flex', justifyContent:'center', alignItems:'center'}} activeStyle={{margin:'5px',height:'120px',width:'120px',color: 'red',border: '3px hotpink solid',display:'flex', justifyContent:'center', alignItems:'center'}} ref="dropzone" onDrop={this.onDrop}>
-                                <div style={{fontSize:'90px', color:'hotpink', marginBottom:'20px'}}>+</div>
+                            <Dropzone style={{margin:'5px',border:'5px #eee solid', width: '140px', height: '220px', display:'flex', justifyContent:'center', alignItems:'center'}} activeStyle={{margin:'5px',height:'120px',width:'120px',color: 'red',border: '3px hotpink solid',display:'flex', justifyContent:'center', alignItems:'center'}} ref="dropzone" onDrop={this.onDrop}>
+                                <div style={{fontSize:'90px', color:'hotpink', marginBottom:'20px'}}>+
+                                  </div>
                             </Dropzone>
                         </div>
                                 {this.state.images.map(function(image,index) {
@@ -130,7 +159,7 @@ var DropzoneCustom = React.createClass({
                   
                   <a className={classNames('btn', 'btn-primary' ,'btn-xs','right')} onClick={this.closeModal}>X</a>
                     <br/>
-                    <img width='700px' src={this.state.images[this.state.modalIndex] && this.state.images[this.state.modalIndex].url} />
+                    <img style={{maxHeight: '500px'}} src={this.state.images[this.state.modalIndex] && this.state.images[this.state.modalIndex].url} />
                     <p>{this.state.images[this.state.modalIndex] && this.state.images[this.state.modalIndex].text}</p>
                 </Modal>
 
